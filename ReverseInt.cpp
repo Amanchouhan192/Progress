e6af03cd6fb5386cafcdfcdf70a6736a71517572
Amanchouhan192 <amanchouhan192@gmail.com>
2020-01-27 19:45:19 +0530

    new file cpp

---
+++ ReverseInt.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverse(int x) {
+        long long int r = 0;
+        while(x!=0)
+        {
+            r = r*10 + (x%10);
+            x = x/10;
+            if(abs(r)>INT_MAX)
+                    return 0;
+        }
+        return r;
+        
+    }
+    
+};
